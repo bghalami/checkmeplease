8e40369c69a8cc99edc1501a02101e1a1c162f96
bghalami <benjaminghalami@gmail.com>
2019-05-20 17:20:37 -0600

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user1 = User.create(name: "Ben", email_address: "ben@ben.com", date_of_birth: DateTime.new(1992, 8, 5));
+user2 = User.create(name: "Ken", email_address: "ken@ken.com", date_of_birth: DateTime.new(1991, 8, 6));
+user3 = User.create(name: "Ren", email_address: "ren@ren.com", date_of_birth: DateTime.new(1993, 8, 7));
+puts "User's seeded"
+user1.state_id = StateId.create(id_number: 4, state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+user2.state_id = StateId.create(id_number: 5, state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+user3.state_id = StateId.create(id_number: 6, state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+puts "State ID's seeded"
+user1.medical_recommendation = MedicalRecommendation.create(recommendation_number: 4, issuer: "A Doctor", state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+user2.medical_recommendation = MedicalRecommendation.create(recommendation_number: 5, issuer: "A Doctor", state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+user3.medical_recommendation = MedicalRecommendation.create(recommendation_number: 6, issuer: "A Doctor", state: "Kansas", expiration_date: DateTime.new(1995, 8, 5), path_to_image: "/file/file/file/file.jpg")
+puts "Medical Recommendations Seeded"
+
+puts "Seeding Complete"
