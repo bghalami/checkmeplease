653582e3c08e4c48cd2e687f2a35054e0dee17e8
bghalami <benjaminghalami@gmail.com>
2019-05-21 17:53:31 -0600

    Added validity check for user_id in URL

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_action :valid_user, only: [:show, :update, :destroy]
+  require 'pry'
+  private
+
+  def valid_user
+    if params[:id]
+      render json: "{\"message\":\"No User found with ID #{params[:id]}\"}" if !user
+    else
+      render json: "{\"message\":\"No User found with ID #{params[:user_id]}\"}" if !user
+    end
+  end
+
+  def user
+    User.find_by_id(params[:id])
+  end
 end
