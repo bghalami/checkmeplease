da918614f8ccf5a3f4ccdaffe8f3c20b2978e445
bghalami <benjaminghalami@gmail.com>
2019-05-21 16:07:44 -0600

    Added EXPIRED to expiration date if expired

--- app/serializers/medical_recommendation_serializer.rb
+++ app/serializers/medical_recommendation_serializer.rb
@@ -1,3 +1,11 @@
 class MedicalRecommendationSerializer < ActiveModel::Serializer
   attributes :recommendation_number, :issuer, :state, :expiration_date, :path_to_image
+
+  def expiration_date
+    if object.expiration_date < DateTime.now.to_date
+      "#{object.expiration_date}(EXPIRED)"
+    else
+      object.expiration_date
+    end
+  end
 end
--- app/serializers/state_id_serializer.rb
+++ app/serializers/state_id_serializer.rb
@@ -1,3 +1,11 @@
 class StateIdSerializer < ActiveModel::Serializer
   attributes :id_number, :state, :expiration_date, :path_to_image
+
+  def expiration_date
+    if object.expiration_date < DateTime.now.to_date
+      "#{object.expiration_date}(EXPIRED)"
+    else
+      object.expiration_date
+    end
+  end
 end
