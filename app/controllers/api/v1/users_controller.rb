1766804ca98c437b238006b8fbb7f3232ec2c20a
bghalami <benjaminghalami@gmail.com>
2019-05-21 14:25:40 -0600

    Made a few testing tweaks

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,5 +3,13 @@ class Api::V1::UsersController < ApplicationController
   def index
     render json: User.all
   end
-  
+
+  def show
+    render json: User.find_by_id(user_params[:id])
+  end
+
+  private
+  def user_params
+    params.permit(:id)
+  end
 end
--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -1,17 +1,35 @@
 require 'rails_helper'
+require 'pry'
+describe "user visits user endpoints" do
+  describe "user sees all users" do
+    describe "they visit '/api/v1/users/" do
+      it "displays all articles" do
+        create_list(:user, 3)
 
-describe "user sees all users" do
-  describe "they visit '/api/v1/users/" do
-    it "displays all articles" do
-      create_list(:user, 3)
+        get '/api/v1/users'
 
-      get '/api/v1/users'
+        expect(response).to be_successful
 
-      expect(response).to be_successful
+        users = JSON.parse(response.body)
 
-      users = JSON.parse(response.body)
-
-      expect(users.count).to eq(3)
+        expect(users.count).to eq(3)
+      end
+    end
+  end
+  describe "user sees single user" do
+    describe "they visit '/api/v1/users/:id" do
+      # it "displays the info of a single user" do
+      #
+      #   user = create(:user)
+      #
+      #   get "/api/v1/users/#{user.id}"
+      #
+      #   expect(response).to be_successful
+      #
+      #   response_user = JSON.parse(response.body)
+      #
+      #   expect(response_user).to eq()
+      # end
     end
   end
 end
