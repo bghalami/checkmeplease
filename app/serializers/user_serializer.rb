bad226b092ebdd13e174da0af53fb1b35ebf560d
bghalami <benjaminghalami@gmail.com>
2019-05-21 16:57:46 -0600

    Changed date_of_birth to only show date, excluding time

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,4 +3,8 @@ class UserSerializer < ActiveModel::Serializer
   has_one :medical_recommendation, serializer: MedicalRecommendationSerializer
 
   attributes :id, :name, :email_address, :date_of_birth, :state_id, :medical_recommendation
+
+  def date_of_birth
+    object.date_of_birth.to_date
+  end
 end
