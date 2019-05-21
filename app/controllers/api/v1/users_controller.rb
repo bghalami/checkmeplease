fb8b7a695779dd708472fb59f4b667ea3d57c2b1
bghalami <benjaminghalami@gmail.com>
2019-05-21 14:54:12 -0600

    Finishing up CRUD functionality on user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,11 +5,40 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    render json: User.find_by_id(user_params[:id])
+    render json: user
+  end
+
+  def update
+    user.update!(user_params)
+    render json: user
+  end
+
+  def create
+    new_user = User.create(user_params)
+    if new_user.save
+      render json: new_user
+    else
+      render error_message("please try again.")
+    end
+  end
+
+  def delete
   end
 
   private
   def user_params
     params.permit(:id)
+  end
+
+  def user
+    User.find_by_id(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :date_of_birth)
+  end
+
+  def error_message(error)
+    "Something went wrong, #{error}"
   end
 end
