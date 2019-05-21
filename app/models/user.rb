2d6c9a5cd9771c4906d3f25b954a0418111db405
bghalami <benjaminghalami@gmail.com>
2019-05-21 16:04:00 -0600

    Added destroy dependents on delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_one :state_id
-  has_one :medical_recommendation
+  has_one :state_id,  dependent: :destroy
+  has_one :medical_recommendation,  dependent: :destroy 
   validates_presence_of :name, :email_address, :date_of_birth
 end
