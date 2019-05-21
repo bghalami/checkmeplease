d69255d4fe059fe253db5fb6b78d99468d46c656
bghalami <benjaminghalami@gmail.com>
2019-05-20 20:10:37 -0600

    Added missing DOB field to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   has_one :state_id, serializer: StateIdSerializer
   has_one :medical_recommendation, serializer: MedicalRecommendationSerializer
 
-  attributes :id, :name, :email_address, :state_id, :medical_recommendation
+  attributes :id, :name, :email_address, :date_of_birth, :state_id, :medical_recommendation
 end
