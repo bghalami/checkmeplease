ec8221a4d1ba8d7211c0df0517a85aa7f6c2752e
bghalami <benjaminghalami@gmail.com>
2019-05-21 17:27:19 -0600

    Added Database Cleaner to test suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -32,6 +32,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+RSpec.configure do |c|
+  c.include Capybara::DSL
+  c.before :each do
+    DatabaseCleaner.clean
+  end
+  c.after :each do
+    DatabaseCleaner.clean
+  end
+end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
