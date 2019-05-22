0699976113fc6828c19887f0724258ffc3df4ab4
bghalami <benjaminghalami@gmail.com>
2019-05-22 14:02:03 -0600

    Refactored controllers to be more secure

--- app/controllers/api/v1/state_ids_controller.rb
+++ app/controllers/api/v1/state_ids_controller.rb
@@ -5,24 +5,34 @@ class Api::V1::StateIdsController < ApplicationController
   end
 
   def update
-    render json: StateId.update_me(state_id, state_id_params)
+    state_id.update!(state_id_params)
+    render json: state_id
   end
 
   def create
-    render json: StateId.create_me(state_id, user_id, state_id_params)
+    if state_id == nil
+      user.state_id = StateId.new(state_id_params)
+      if user.state_id.save
+        render json: state_id
+      else
+      render json: "{\"message\": \"Something went wrong, please try again.\"}"
+      end
+    else
+      render json: "{\"message\": \"User #{user.id} already has a State ID\"}"
+    end
   end
 
   def destroy
-    render json: StateId.delete_me(state_id, user_id)
+    render json: "{\"message\": \"StateId for User #{user.id} successfully deleted\"}" if state_id.destroy
   end
 
   private
-  def user_id
-    params.permit(:user_id)[:user_id]
+  def user
+    User.find_by_id(params[:user_id])
   end
 
   def state_id
-    StateId.find_by(user_id: user_id)
+   user.state_id
   end
 
   def state_id_params
--- app/models/state_id.rb
+++ app/models/state_id.rb
@@ -1,27 +1,4 @@
 class StateId < ApplicationRecord
   validates_presence_of :id_number, :state, :expiration_date, :path_to_image
   belongs_to :user
-
-  def self.delete_me(state_id, user_id)
-    "{\"message\": \"StateId for User #{user_id} successfully deleted\"}" if state_id.destroy
-  end
-
-  def self.update_me(state_id, state_id_params)
-    state_id.update!(state_id_params)
-    state_id
-  end
-
-  def self.create_me(state_id, user_id, state_id_params)
-    user = User.find_by_id(user_id)
-    if user.state_id == nil
-      user.state_id = StateId.create(state_id_params)
-      if user.state_id.save
-        user.state_id
-      else
-        "{\"message\": \"Something went wrong, please try again.\"}"
-      end
-    else
-      "{\"message\": \"User #{user_id} already has a State ID\"}"
-    end
-  end
 end
